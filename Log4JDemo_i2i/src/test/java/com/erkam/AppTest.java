a2b14ce3d0cbe601c33aefc555cfc34623c2526d
Erkam <98554437+erkamdogrul@users.noreply.github.com>
2023-07-19 17:09:36 +0300

    Add files via upload

---
+++ Log4JDemo_i2i/src/test/java/com/erkam/AppTest.java
@@ -0,0 +1,16 @@
+package com.erkam;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
